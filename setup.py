aa83d597ea488d4cf2880424ae98d1d4cd1c9501
Mohammed Alfaifi <msasdf1@gmail.com>
2021-02-10 00:43:46 +0300

    therd

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name='first_degree_equation',
-    version='',
+    version='1.0.0',
     packages=['ep_final'],
     url='https://github.com/mathsprog/first_degree_equation.git',
     license='101',
